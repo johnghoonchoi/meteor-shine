7a06018590425cd79c64dced1c9a3542f7c17c58
DavidSunny <uuunet1@gmail.com>
2015-08-06 15:00:42 +0900

    Config setting up about meetup

--- apps/front/server/config/accounts.js
+++ apps/front/server/config/accounts.js
@@ -38,8 +38,8 @@ ServiceConfiguration.configurations.remove({
 if (ServiceConfiguration.configurations.find({service: 'meetup'}).count() === 0) {
   ServiceConfiguration.configurations.insert({
     service: 'meetup',
-    clientId: Meteor.settings.meetup.clientId,
-    secret: Meteor.settings.meetup.secret
+    clientId: Meteor.settings.meetup.oauthKey,
+    secret: Meteor.settings.meetup.oauthSecret
   });
 }
 
@@ -50,16 +50,64 @@ if (ServiceConfiguration.configurations.find({service: 'meetup'}).count() === 0)
  */
 Accounts.onCreateUser(function(options, user) {
   console.log('options: ' + JSON.stringify(options, null, 2));
-  console.log('user:' + JSON.stringify(user, null, 2));
+  Logger.info('options: ' + JSON.stringify(options, null, 2));
 
-  if (options.profile)
+  if (user.services.facebook) {
+    var userData = user.services.facebook;
+    var userFacebookId= userData.id;
+    var name= userData.name;
+    var thumbnailUrl = 'http://graph.facebook.com/' + userFacebookId + '/picture?type=square&height=160&width=160';
+
+    options.profile = {
+      'name': userData.name,
+      'facebookProfileUrl': userData.link,
+      'thumbnailUrl': thumbnailUrl
+    };
+
     user.profile = options.profile;
+  }
 
-  //if (user.services.facebook) {
-  //  var id= user.services.facebook.id;
-  //  var img = 'http://graph.facebook.com/' + id + '/picture?type=square&height=160&width=160';
-  //  user.profile.url = img;
-  //}
+  if (user.services.meetup) {
+    var userMeetupId = user.services.meetup.id;
+    var apiKey = Meteor.settings.meetup.apiKey;
+    var requestUrl = 'https://api.meetup.com/2/member/' + userMeetupId + '?key=' + apiKey + '&signed=true&fields=other_services';
+    var response = HTTP.get(requestUrl, {
+      params: {
+        format: 'json'
+      }
+    });
+
+    var userData = response.data;
+    if(userData.hasOwnProperty("photo") && userData.photo.photo_link !== "") {
+      var thumbnailUrl = userData.photo.photo_link;
+    } else {
+      var thumbnailUrl = "/default-avatar.png";
+    }
+
+    var socialLinks = [];
+    for (service in userData.other_services) {
+      if(service === "twitter") {
+        var username = userData.other_services['twitter']['identifier'];
+        socialLinks.push({'service': 'twitter', 'url': 'https://twitter.com/' + username});
+      } else if(service) {
+        var url = userData.other_services[service]['identifier'];
+        socialLinks.push({'service': service, 'url': url});
+      }
+    }
+
+    options.profile = {
+      'name': userData.name,
+      'meetupProfileUrl': userData.link,
+      'thumbnailUrl': thumbnailUrl,
+      'bio': userData.bio,
+      'socialLinks': socialLinks
+    };
+    user.profile = options.profile;
+  }
+
+
+  console.log('user:' + JSON.stringify(user, null, 2));
+  Logger.info('user:' + JSON.stringify(user, null, 2));
 
   return user;
 
