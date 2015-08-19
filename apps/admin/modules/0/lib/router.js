5d86d46cf2f5f5c044dd4e8e26c322bec6be8e82
Lee Sangwon <leesn@bookp.al>
2015-08-19 11:58:59 +0900

    admin routing code clear

--- apps/admin/modules/0/lib/router.js
+++ apps/admin/modules/0/lib/router.js
@@ -1,46 +1,20 @@
 
 Router.configure({
   layoutTemplate: 'layout',
-  loadingTemplate: 'loading',
-  waitOn: function() {
-    Meteor.subscribe("userStatus");
-    Meteor.subscribe('profileView');
-
-    return (Meteor.userId()) ? [ Meteor.subscribe('notificationsList') ] : [];
-  }
+  loadingTemplate: 'loading'
 });
 
-var accessControl = function() {
+Router.onBeforeAction(function() {
   if (! Meteor.user()) {
     if (Meteor.loggingIn()) {
       this.render(this.loadingTemplate);
     } else {
       Accounts.ui.dialog.show('signIn');
-      //this.next();
     }
   } else {
     this.next();
   }
-};
-
-var closeAsideMobile = function () {
-  if ($('#wrapper').hasClass('mobile')) {
-    if ($('#wrapper').hasClass('aside-left-set')) {
-      $('#wrapper').removeClass('aside-left-set');
-    }
-    if ($('#wrapper').hasClass('aside-right-set')) {
-      $('#wrapper').removeClass('aside-right-set');
-    }
-    this.next();
-  } else {
-    this.next();
-  }
-};
-
-Router.onBeforeAction(closeAsideMobile);
-
-Router.onBeforeAction(accessControl);
-
+});
 
 Router.route('/', function() {
   this.redirect('/home');
