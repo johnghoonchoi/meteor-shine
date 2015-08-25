95df57b35724a9592652d1543a6d953e45735aea
SungYeolWoo <altari1415@gmail.com>
2015-08-25 16:29:00 +0900

    refactoring style

--- apps/front/client/lib/scroll_to_bottom.js
+++ apps/front/client/lib/scroll_to_bottom.js
@@ -5,9 +5,5 @@
 ScrollToBottom = function (selector) {
   // scroll to bottom of element
   var element = $(selector)[0];
-  console.log('element', element);
-  console.log('element.scrollTop_before', element.scrollTop);
-  console.log('element.scrollHeight', element.scrollHeight);
   element.scrollTop = 999999;
-  console.log('element.scrollTop_after', element.scrollTop);
 };
