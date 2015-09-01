8a44d4d325e81e3e078d72d49a89733e19cc03ba
SungYeolWoo <altari1415@gmail.com>
2015-09-01 15:19:45 +0900

    change source code that ES6 and change css

--- apps/front/modules/chat/client/chat_frame.js
+++ apps/front/modules/chat/client/chat_frame.js
@@ -6,36 +6,26 @@
 // chatView
 Template.chatFrame.onCreated(function () {
   // initialize
-  var instance = this;
-  var data = Template.currentData();
-  var toId = data.user._id;
+  let data = Template.currentData();
+  let toId = data.user._id;
 
   // other side input message
-  instance.status = "input";
-  instance.onTyping = false;
+  this.status = "input";
+  this.onTyping = false;
 
-  Meteor.call('chatStatusRemove', instance.status);
+  Meteor.call('chatStatusRemove', this.status);
 
-  // subscribe
-  instance.autorun(function () {
-    instance.chatMessages = instance.subscribe('chatMessages', toId);
-    instance.subscribe('chatStatus', toId, instance.status);
-
+  this.autorun( () => {
+    this.chatMessages = this.subscribe('chatMessages', toId);
+    this.subscribe('chatStatus', toId, this.status);
   });
-
-  instance.autorun(function () {
-    // when subscribe is ready
-    if (instance.chatMessages.ready()) {
-      var result = Meteor.users.findOne({_id: toId});
-      // getPicture
-      instance.partnerPicture = getPicture(result);
-    }
+  
+  this.autorun( () => {
+    if (this.chatMessages.ready())
+      this.partnerPicture = getPicture(Meteor.users.findOne({_id: toId}));
   });
-
-  instance.latelyDate = function () {
-    return ChatMessages.findOne({}, { sort: { createdAt: -1 }});
-  };
-
+  this.latelyDate = () => ChatMessages.findOne({}, { sort: { createdAt: -1 }});
+  
 });
 
 Template.chatFrame.onDestroyed(function () {
@@ -48,11 +38,10 @@ Template.chatFrame.onDestroyed(function () {
 });
 
 Template.chatFrame.helpers({
-  chatMessagesList: function () {
-      return ChatMessages.find({}, { sort : { createdAt: 1 } });
+  chatMessagesList () {
+    return ChatMessages.find({}, { sort : { createdAt: 1 } });
   },
-
-  onTyping: function () {
+  onTyping () {
     return Counts.get('chatStatusCount') > 0 ? true : false;
   }
 });
@@ -60,21 +49,21 @@ Template.chatFrame.helpers({
 Template.chatFrame.events({
 
   // header events
-  'click a.close': function (e) {
+  'click a.close' (e) {
     Blaze.remove(chatSingleton.template);
   },
 
   // footer events
-  'keyup textarea': function (e, instance) {
+  'keyup textarea' (e, instance) {
 
-    var thisElement = instance.find("textarea");
-    var content = thisElement.value;
+    let thisElement = instance.find("textarea");
+    let content = thisElement.value;
 
     // remove line breaks from string
     content = content.replace(/(\r\n|\n|\r)/gm,"");
 
-    var toId = this.user._id;
-    var data = {
+    let toId = this.user._id;
+    let data = {
       toId: toId,
       status: instance.status
     };
@@ -102,17 +91,19 @@ Template.chatFrame.events({
       e.stopPropagation();
       e.preventDefault();
 
+
+      console.log('content', content);
       // clear textarea
       thisElement.value = "";
 
       // lately date to less than 5 minutes date.
-      var latelyDate = instance.latelyDate() || 0;
+      let latelyDate = instance.latelyDate() || 0;
 
-      var inputDate = new Date();
+      let inputDate = new Date();
 
-      var diffMinutes = Math.abs(moment(latelyDate.createdAt).diff(inputDate, "minutes"));
-      console.log('diffMinutes', diffMinutes);
-      var timeScope = 10;
+      let diffMinutes = Math.abs(moment(latelyDate.createdAt).diff(inputDate, "minutes"));
+
+      let timeScope = 10;
 
       if (!latelyDate || diffMinutes >= timeScope) {
         // insert message (type=date)
@@ -123,7 +114,7 @@ Template.chatFrame.events({
         Meteor.call('chatMessageInsert', data);
       }
 
-      var data = {
+      let data = {
         toId: toId,
         content: content,
         type: "msg"
@@ -143,20 +134,20 @@ Template.chatFrame.events({
 // chatMessageListItem
 Template.chatMessageListItem.onRendered(function () {
   // scroll to bottom of main div
-  var selector = 'main.body';
+  let selector = 'main.body';
   ScrollToBottom(selector);
 });
 
 Template.chatMessageListItem.helpers({
-  isDate: function (type) {
+  isDate (type) {
     return type === "date";
   },
 
-  isUserMessage: function (from) {
+  isUserMessage (from) {
     return Meteor.userId() === from._id ? true : false;
   },
 
-  getPartnerPictures: function () {
+  getPartnerPictures () {
     return Template.instance().parentInstance("chatFrame").partnerPicture;
   }
 });
@@ -165,12 +156,12 @@ Template.chatMessageListItem.helpers({
 // chatStatusListItem
 Template.chatStatusInput.onRendered(function () {
   // scroll to bottom of main div
-  var selector = 'main.body';
+  let selector = 'main.body';
   ScrollToBottom(selector);
 });
 
 Template.chatStatusInput.helpers({
-  getPartnerPictures: function () {
+  getPartnerPictures () {
     return Template.instance().parentInstance("chatFrame").partnerPicture;
   }
 });
